12ba99276bfccd50f335122886ac6e53c68e5a9c
George <loaizageorge@gmail.com>
2016-07-22 18:33:51 -0400

    Create forEach loop to get and save info from array of streamers

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
+    var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    var titles = [];
+    var descriptions =[];
+    var logoUrls = [];
+    var urls = [];
+    var user  = streamers[0];
     
-    $.getJSON('https://api.twitch.tv/kraken/streams/eleaguetv?callback=?',function(data){
-        console.log(data.stream.game);
+    
+    streamers.forEach(function(streamer){
+    
+    $.getJSON("https://api.twitch.tv/kraken/streams/"+streamer+"?callback=?",function(data){
+        alert(streamer);
+        //console.log(data.stream);
+        //var spaghetti = JSON.stringify(data.stream);
+        //$("#streamer").html(data.stream.channel.display_name);
+        //$("#description").html(data.stream.channel.status);
+        
     });
+});
+
 });
