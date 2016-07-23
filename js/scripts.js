62bedc03eee4e75d9520f4ce09fa997f71dbb516
George <loaizageorge@gmail.com>
2016-07-23 00:03:44 -0400

    Scracth the array stuff. Append html w/ jQuery for offline channels.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+   // var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    var streamers = ["freecodecamp","storbeck", "habathcx"]; 
     var titles = [];
     var descriptions =[];
     var logoUrls = [];
@@ -9,14 +10,47 @@ $(document).ready(function(){
     
     streamers.forEach(function(streamer){
     
-    $.getJSON("https://api.twitch.tv/kraken/streams/"+streamer+"?callback=?",function(data){
-        alert(streamer);
-        //console.log(data.stream);
-        //var spaghetti = JSON.stringify(data.stream);
-        //$("#streamer").html(data.stream.channel.display_name);
-        //$("#description").html(data.stream.channel.status);
+    $.getJSON("https://api.twitch.tv/kraken/streams/freecodecamp?callback=?",function(streamInfo){
+        
+        if(streamInfo.stream==null){
+            offlineStream(streamer);
+        }
+        else{
+            onlineStream(streamInfo);
+        }
+        
+       
+       
+        
+        
         
     });
+        
+         //console.log(data.stream);
+        //var spaghetti = JSON.stringify(data);
+        //$("#streamer").html(data.stream.channel.display_name);
+        //$("#description").html(data.stream.channel.status);
+       
 });
+   
 
 });
+
+function offlineStream(streamer){
+    $.getJSON("https://api.twitch.tv/kraken/channels/"+streamer+"?callback=?",function(channelInfo){
+        
+        //console.log(JSON.stringify(channelInfo));
+        //console.log(channelInfo.logo);
+        //$(".content").append("<h3>"+channelInfo.+"</h3>")
+        $(".content").append("<a href = "+channelInfo.url+"> "+streamer+" </a> ");
+        $(".content").append("<img src = "+channelInfo.logo+">");
+        
+        });
+    
+    
+}
+
+function onlineStream(data){
+    
+}
+
